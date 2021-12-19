88f4db7486b6f48cf681925177107e050aedf2c0
paolaborda <nelly.borda.mt@correo.usa.edu.co>
2021-12-18 22:11:50 -0600

    actualizacion

---
+++ RETO2_CICLO4-master/src/main/java/RETO2_CICLO4/repositorio/FootwearRepositorio.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RETO2_CICLO4.repositorio;
+
+import RETO2_CICLO4.interfaz.InterfaceFootwear;
+import RETO2_CICLO4.modelo.Footwear;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author nelly borda
+ */
+@Repository
+public class FootwearRepositorio {
+    @Autowired
+    private InterfaceFootwear InterfaceOrder;
+    
+public List<Footwear> getAll() {
+        return InterfaceOrder.findAll();
+    }
+
+    public Optional<Footwear> getFootwears(String reference) {
+        return InterfaceOrder.findById(reference);
+    }
+    public Footwear create(Footwear Footwear) {
+        return InterfaceOrder.save(Footwear);
+    }
+
+    public void update(Footwear Footwear) {
+        InterfaceOrder.save(Footwear);
+    }
+    
+    public void delete(Footwear Footwear) {
+        InterfaceOrder.delete(Footwear);
+    }
+}
+    
+    
+    
+
